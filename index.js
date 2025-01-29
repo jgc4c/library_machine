26fd536f5241bd85d4c5ca54fc3945ca8143ca6e
geeb <jgc4c@mtmail.mtsu.edu>
2025-01-29 11:57:42 -0600

    added nodejs connections to mysql database. Inlcuded some test queries for adding and viewing data.

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 var http = require('http');
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({ // creates the connection to your database
+	host: "localhost", // IP address of where the database is, localhost if you're doing it locally
+	user: "geeb", // database user account you want to use. make sure it's a user 
+	password: "pingu", // password for database user account
+	database: "testing" // name of database to connect to
+});
+
+con.connect(function(err) {
+	if (err) throw err;
+	console.log("connected");
+
+	con.query("INSERT INTO game VALUES (\"Minecraft\", \"2009-5-17\")", function (err, result, fields) {
+		if (err) throw err;
+		console.log(result);
+	});
+
+	con.query("SELECT * FROM game", function (err, result, fields) {
+		if (err) throw err;
+		console.log(result);
+	});
+});
 
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
